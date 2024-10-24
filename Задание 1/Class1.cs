9d2c42399fbc1b08d0faaf9bc5428b88404bf31b
Elya-vol13 <145384328+Elya-vol13@users.noreply.github.com>
2024-10-24 19:18:16 +0500

    Add files via upload

---
+++ Задание 1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_csharp
+{
+    internal class Animals
+    {
+        //поля
+        private string name;
+
+        ////конструктор 
+        public Animals(string name)
+        {
+            this.name = name;
+        }
+
+        //конструктор копирования
+        public Animals(Animals animals)
+        {
+            this.name = animals.name;
+        }
+
+        //метод добавления восклицательных знаков в начало строки
+        public void add_ex()
+        {
+            name = "!!!" + name;
+        }
+
+        //перегрузка метода ToString
+        public override string ToString()
+        {
+            return $"Животное: {name}";
+        }
+
+        //свойство для доступа к приватному полю главного класса
+        public string Name => name;
+    }
+}
