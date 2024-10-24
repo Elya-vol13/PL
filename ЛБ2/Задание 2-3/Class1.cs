57fcc5bd425e767c2c411eded5a67e8f808d1a46
Elya-vol13 <145384328+Elya-vol13@users.noreply.github.com>
2024-10-24 21:40:15 +0500

    Add files via upload

---
+++ ЛБ2/Задание 2-3/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_csharp
+{
+    internal class Money
+    {
+        //поля
+        private uint rubles;
+        private byte kopeks;
+
+        //конструктор
+        public Money(uint rubles, byte kopeks)
+        {
+            this.rubles = rubles;
+            this.kopeks = kopeks;
+        }
+
+        //метод для добавления копеек
+        public Money AddKopeks(uint addKopeks)
+        {
+            uint totalKopeks = this.kopeks + addKopeks;
+            uint newRubles = this.rubles + totalKopeks / 100;
+            byte newKopeks = (byte)(totalKopeks % 100);
+            return new Money(newRubles, newKopeks);
+        }
+        
+
+        //перегрузка метода ToString()
+        public override string ToString()
+        {
+            return $"{rubles} руб. {kopeks} коп.";
+        }
+
+        //задание 3
+        //унарные операции
+        public static Money operator ++(Money m)
+        {
+            return m.AddKopeks(1);
+        }
+
+        public static Money operator --(Money m)
+        {
+            if (m.kopeks == 0)
+            {
+                if (m.rubles == 0)
+                    return new Money(0, 0);
+                return new Money(m.rubles - 1, 99);
+            }
+            return new Money(m.rubles, (byte)(m.kopeks - 1));
+        }
+
+        //операции приведения типа
+        public static explicit operator uint(Money m)
+        {
+            return m.rubles;
+        }
+
+        public static implicit operator double(Money m)
+        {
+            return m.kopeks / 100.0;
+        }
+
+        //бинарные операции с беззнаковыми целыми числами
+        public static Money operator +(Money m, uint addKopeks)
+        {
+            return m.AddKopeks(addKopeks);
+        }
+
+        public static Money operator +(uint addKopeks, Money m)
+        {
+            return m.AddKopeks(addKopeks);
+        }
+
+        public static Money operator -(Money m, uint subKopeks)
+        {
+            if (subKopeks > m.rubles * 100 + m.kopeks)
+            {
+                return new Money(0, (byte)0);
+            }
+            uint totalKopeks = (m.rubles * 100 + m.kopeks) - subKopeks;
+            return new Money(totalKopeks / 100, (byte)(totalKopeks % 100));
+        }
+
+        public static Money operator -(uint subKopeks, Money m)
+        {
+            
+            if (subKopeks < m.rubles * 100 + m.kopeks)
+            {
+                return new Money(0, (byte)0);
+            }
+            uint totalKopeks = subKopeks - (m.rubles * 100 + m.kopeks);
+            return new Money(totalKopeks / 100, (byte)(totalKopeks % 100));
+        }
+    }
+}
---
+++ ЛБ2/Задание 2-3/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод данных пользователем
+            Console.WriteLine("Введите рубли: ");
+            uint rubles;
+            while (true)
+            {
+                if (uint.TryParse(Console.ReadLine(), out rubles))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+
+            Console.WriteLine("Введите копейки: ");
+            byte kopeks;
+            while (true)
+            {
+                if (byte.TryParse(Console.ReadLine(), out kopeks))
+                {
+                    if (kopeks < 100)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите число до 100!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+            //создание объекта Money
+            Money money = new Money(rubles, kopeks);
+            Console.WriteLine("Исходная сумма: " + money);
+
+            //задание 2
+            Console.WriteLine("\nЗадание 2:");
+            //ввод дополнительных копеек
+            Console.WriteLine("Введите количество добавляемых копеек: ");
+            uint addKopeks;
+            while (true)
+            {
+                if (uint.TryParse(Console.ReadLine(), out addKopeks))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+            
+            //добавление копеек и вывод результата
+            Money updatedMoney = money.AddKopeks(addKopeks);
+            Console.WriteLine("Обновленная сумма: " + updatedMoney);
+
+            //задание 3
+            Console.WriteLine("\nЗадание 3:");
+
+            //тестирование унарных операций
+            Console.WriteLine("\nТестирование унарных операций:");
+            Console.WriteLine("После добавления копейки: " + ++money);
+            Console.WriteLine("После вычитания копейки: " + --money);
+
+            //тестирование операций приведения типов
+            Console.WriteLine("\nТестирование операций приведения типов:");
+            uint rubleAmount = (uint)money; // Явное приведение
+            double kopeckAmount = money;     // Неявное приведение
+            Console.WriteLine($"Количество рублей: {rubleAmount}");
+            Console.WriteLine($"Количество копеек в рублях: {kopeckAmount}");
+
+            //тестирование бинарных операций
+            Console.WriteLine("\nТестирование бинарных операций:");
+            Console.WriteLine("Сколько копеек вы хотите добавить:");
+            uint x;
+            while (true)
+            {
+                if (uint.TryParse(Console.ReadLine(), out x))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+            Console.WriteLine($"Добавление {x} копеек: " + (money + x));
+
+            Console.WriteLine("Сколько копеек вы хотите вычесть:");
+            while (true)
+            {
+                if (uint.TryParse(Console.ReadLine(), out x))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+            Console.WriteLine($"Вычитание {x} копеек: " + (money - x));
+
+            Console.WriteLine("Ко скольки копейкам вы хотите добавить:");
+            while (true)
+            {
+                if (uint.TryParse(Console.ReadLine(), out x))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+            Console.WriteLine($"Добавление к {x}: " + (x + money));
+
+            Console.WriteLine("Из скольки копеек вы хотите вычесть:");
+            while (true)
+            {
+                if (uint.TryParse(Console.ReadLine(), out x))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+            Console.WriteLine($"Вычитание из {x} копеек: " + (x - money));
+            Console.ReadKey();
+        }
+    }
+}
