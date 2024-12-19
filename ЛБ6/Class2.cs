e1f8459c6ccf5d82ec09c0770411721bfa2c7423
Elya-vol13 <145384328+Elya-vol13@users.noreply.github.com>
2024-12-20 01:01:18 +0500

    Add files via upload

---
+++ ЛБ6/Class2.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace ConsoleApp1_csharp
+{
+    
+
+    public class Fraction : ICloneable
+    {
+        private int num; //Числитель
+        private int den; //Знаменатель
+
+        //Конструктор
+        public Fraction(int num1, int den1)
+        {
+            num = num1;
+            den = den1;
+            Normalize(); //Сокращаем дробь
+        }
+
+        //Метод для сокращения дроби
+        private void Normalize()
+        {
+            int nod = NOD(Math.Abs(num), den);
+            num /= nod;
+            den /= nod;
+        }
+
+        //Метод для нахождения НОД
+        private int NOD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int c = b;
+                b = a % b;
+                a = c;
+            }
+            return a;
+        }
+
+        //Строковое представление дроби
+        public override string ToString()
+        {
+            return $"{num}/{den}";
+        }
+
+        //1
+        //Перегрузки арифметических операции
+        //Операции между дробями
+        //Cложениe
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            int new_num = a.num * b.den + b.num * a.den;
+            int new_den = a.den * b.den;
+            return new Fraction(new_num, new_den);
+        }
+
+        //Вычитание
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            int new_num = a.num * b.den - b.num * a.den;
+            int new_den = a.den * b.den;
+            return new Fraction(new_num, new_den);
+        }
+
+        //Умножение
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            int new_num = a.num * b.num;
+            int new_den = a.den * b.den;
+            return new Fraction(new_num, new_den);
+        }
+
+        //Деление
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.num == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно.");
+
+            int new_num = a.num * b.den;
+            int new_den = a.den * b.num;
+            return new Fraction(new_num, new_den);
+        }
+
+        //Операции с целым числом
+        //Сложение
+        public static Fraction operator +(Fraction a, int B)
+        {
+            Fraction b = new Fraction(B, 1);
+            return a + b; //Используем уже перегруженный оператор +
+        }
+
+        //Вычитание
+        public static Fraction operator -(Fraction a, int B)
+        {
+            Fraction b = new Fraction(B, 1);
+            return a - b; //Используем уже перегруженный оператор -
+        }
+
+        //Умножение
+        public static Fraction operator *(Fraction a, int b)
+        {
+            return new Fraction(a.num * b, a.den);
+        }
+
+        //Деление
+        public static Fraction operator /(Fraction a, int b)
+        {
+            return new Fraction(a.num, a.den * b);
+        }
+
+        //2
+        //Перегрузка операторов сравнения
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            if(a.num == b.num){
+                if (a.den == b.den)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            if (a.num == b.num)
+            {
+                if (a.den == b.den)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //3
+        public object Clone()
+        {
+            //Возвращаем новый объект Fraction с теми же значениями полей
+            return new Fraction(num, den);
+        }
+
+        //4
+        //Метод для получения вещественного значения
+        public double GetValue()
+        {
+            return (double)num / den;
+        }
+
+        //Метода для установки числителя и знаменателя
+        public void SetFraction(int num, int den)
+        {
+            this.num = num;
+            this.den = den;
+        }
+    }
+}
---
+++ ЛБ6/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Выберите задание:\n1. Задание 1\n2. Задание 2\n0. Выход\n>>>");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        //Задание 1
+                        Console.WriteLine("\nЗадание 1:");
+                        //1
+                        Console.WriteLine("1:");
+                        string c;
+                        //Ввод имени кота
+                        while (true)
+                        {
+                            Console.Write("Введите имя кота: ");
+                            c = Console.ReadLine();
+                            if (string.IsNullOrWhiteSpace(c))
+                            {
+                                Console.WriteLine("Имя кота не может быть пустым или состоять только из пробелов.");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        Cat c1 = new Cat(c);
+                        Console.WriteLine($"{c1.ToString()}");
+                        c1.Meow(); //Мяукает один раз
+                        c1.Meow(3); //Мяукает три раза
+
+                        //2
+                        Console.WriteLine("\n2:");
+                        Cat c2 = new Cat("Мурзик");
+                        ToyCat toyCat = new ToyCat("Плюшевый кот");
+
+                        //Собираем все мяукающие объекты в список
+                        List<IMeow> ms = new List<IMeow> { c1, c2, toyCat };
+
+                        AllMeow(ms);
+
+                        //3
+                        Console.WriteLine("\n3:");
+                        // Создаем список с одним котом
+                        CountingCat C1 = new CountingCat(c1);
+                        List<CountingCat> ms1 = new List<CountingCat> { C1 };
+                        for (int i = 0; i < 5; i++)
+                        {
+                            AllMeow(ms1);
+                        }
+                        AllMeow(ms1);
+                        Console.WriteLine($"Количество мяуканий: {C1.MeowCount()}");
+                        break;
+                    
+                    case "2":
+                        //Задание 2
+                        Console.WriteLine("\nЗадание 2:");
+                        //1
+                        Console.WriteLine("1:");
+                        // Создание экземпляров дробей
+                        int ch, z;
+                        Console.WriteLine("Введите первую дробь:");
+                        ch = Input_num();
+                        z = Input_den();
+                        Fraction f1 = new Fraction(ch, z);
+                        Console.WriteLine("\nВведите вторую дробь:");
+                        ch = Input_num();
+                        z = Input_den();
+                        Fraction f2 = new Fraction(ch, z);
+                        Console.WriteLine("\nВведите третью дробь:");
+                        ch = Input_num();
+                        z = Input_den();
+                        Fraction f3 = new Fraction(ch, z);
+
+                        //Дроби
+                        Console.WriteLine("\nВаши дроби:");
+                        Console.WriteLine($"f1: {f1}");
+                        Console.WriteLine($"f2: {f2}");
+                        Console.WriteLine($"f3: {f3}");
+
+                        // Примеры использования методов
+                        Console.WriteLine($"{f1} + {f2} = {f1 + f2}");
+                        Console.WriteLine($"{f1} - {f2} = {f1 - f2}");
+                        Console.WriteLine($"{f1} * {f2} = {f1 * f2}");
+                        Console.WriteLine($"{f1} / {f2} = {f1 / f2}");
+                        Console.WriteLine($"{f1} + {13} = {f1 + 13}");
+                        Console.WriteLine($"{f2} - {5} = {f2 - 5}");
+                        Console.WriteLine($"{f3} * {4} = {f3 * 4}");
+                        Console.WriteLine($"{f1} / {2} = {f1 / 2}");
+
+                        // Пример вычисления f1.sum(f2).div(f3).minus(5)
+                        Console.WriteLine($"Результат (f1 + f2) / f3 - 5: {(f1 + f2) / f3 - 5}");
+
+                        //2
+                        Console.WriteLine("\n2:");
+                        Console.WriteLine($"{f1} == {f2}: {f1 == f2}");
+                        Console.WriteLine($"{f1} == {f3}: {f1 == f3}"); 
+                        Console.WriteLine($"{f2} != {f3}: {f2 != f3}");
+
+                        //3
+                        Console.WriteLine("\n3:");
+                        Fraction f4 = (Fraction)f3.Clone();
+                        Console.WriteLine($"f3 = {f3}");
+                        Console.WriteLine($"f4(копия f4) = {f4}");
+                        Console.WriteLine($"{f4} == {f3}: {f4 == f3}");
+
+                        //4
+                        Console.WriteLine("\n3:");
+                        //Получаем вещественное значение дроби
+                        Console.WriteLine("Значение дроби f1: " + f1.GetValue());
+                        //Устанавливаем новые значения для числителя и знаменателя
+                        f1.SetFraction(5, 2);
+                        Console.WriteLine($"f1 после установки новых числителя и знаменателя = {f1}");
+
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Нет такой опции!");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Для задания 1
+        public static void AllMeow(IEnumerable<IMeow> ms)
+        {
+            foreach (var m in ms)
+            {
+                m.Meow();
+            }
+        }
+
+        //Для задания 2
+        //Ввод числителя
+        static int Input_num()
+        {
+            Console.Write("Введите числитель: ");
+            int a;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out a))
+                {
+                    return a;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+        }
+        //Ввод знаменателя
+        static int Input_den()
+        {
+            Console.Write("Введите знаменатель: ");
+            int a;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out a))
+                {
+                    if (a > 0)
+                    {
+                        return a;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Знаменатель должен быть больше 0!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод! Введите целое число!");
+                }
+            }
+        }
+    }
+}
