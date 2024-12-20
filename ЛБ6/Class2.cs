1def2838ca6b8152a58bf435a2c696c4746086ef
Elya-vol13 <145384328+Elya-vol13@users.noreply.github.com>
2024-12-20 11:45:49 +0500

    Update Class2.cs

--- ЛБ6/Class2.cs
+++ ЛБ6/Class2.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace ConsoleApp1_csharp
 {
-    // Интерфейс для дроби
+    //Интерфейс для дроби
     public interface IFraction
     {
-        // Метод для получения вещественного значения дроби
+        //Метод для получения вещественного значения дроби
         double GetValue();
 
-        // Метод для установки числителя и знаменателя
+        //Метод для установки числителя и знаменателя
         void SetFraction(int numerator, int denominator);
     }
 
